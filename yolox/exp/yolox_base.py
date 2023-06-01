f1e7c7abb477ce5d7038beb2369dbef387bb662e
naihai78 <757877430@qq.com>
2023-06-01 21:11:39 +0800

    "代码重构"

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -109,7 +109,7 @@ class Exp(BaseExp):
         self.nmsthre = 0.65
 
     def get_model(self):
-        from yolox.models import YOLOX, YOLO_Repvgg, YOLOXHead
+        from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
 
         def init_yolo(M):
             for m in M.modules():
@@ -118,9 +118,9 @@ class Exp(BaseExp):
                     m.momentum = 0.03
 
         if getattr(self, "model", None) is None:
-            in_channels = [256, 256]  # in channels for head
-            strides = [8, 16]  # p2 p3 p4
-            backbone = YOLO_Repvgg(self.depth, self.width)
+            in_channels = [256, 512, 1024]  # in channels for head
+            strides = [8, 16, 32]  # p2 p3 p4
+            backbone = YOLOPAFPN(self.depth, self.width)
             head = YOLOXHead(self.num_classes, self.width, strides=strides, in_channels=in_channels, act=self.act)
             self.model = YOLOX(backbone, head)
 

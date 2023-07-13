59ee91cc827acaf9767ed888985e59b59635ae92
naihai78 <757877430@qq.com>
2023-07-13 14:39:06 +0800

    "实验修改训练"

--- tools/train.py
+++ tools/train.py
@@ -14,6 +14,9 @@ from yolox.core import launch
 from yolox.exp import Exp, check_exp_value, get_exp
 from yolox.utils import configure_module, configure_nccl, configure_omp, get_num_devices
 
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX train parser")

4f17ce934c0513694ee846141b7e9d6c5619df31
naihai78 <757877430@qq.com>
2023-07-27 21:39:01 +0800

    "稀疏训练 sr = 0.0002"

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -113,7 +113,7 @@ class Exp(BaseExp):
         self.sr = 0.0002  # L1 normal sparse rate
 
     def get_model(self):
-        from yolox.models import YOLOX, YOLOPAFPN_P2, YOLOXHead
+        from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
 
         def init_yolo(M):
             for m in M.modules():
@@ -122,9 +122,9 @@ class Exp(BaseExp):
                     m.momentum = 0.03
 
         if getattr(self, "model", None) is None:
-            in_channels = [256, 256, 512]  # in channels for head
-            strides = [4, 8, 16]  # p2 p3 p4
-            backbone = YOLOPAFPN_P2(self.depth, self.width)
+            in_channels = [256, 512, 1024]  # in channels for head
+            strides = [8, 16, 32]  # p2 p3 p4
+            backbone = YOLOPAFPN(self.depth, self.width)
             head = YOLOXHead(self.num_classes, self.width, strides=strides, in_channels=in_channels, act=self.act)
             self.model = YOLOX(backbone, head)
 

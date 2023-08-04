2519303e87dab176493fc8d16668eb58f208dc22
naihai78 <757877430@qq.com>
2023-08-04 20:39:00 +0800

    "常规训练"

--- yolox/models/yolo_pafpn.py
+++ yolox/models/yolo_pafpn.py
@@ -658,8 +658,8 @@ class YOLOPAFPN_Rep(YOLOPAFPN):
         )
 
         # bottom-up conv
-        self.bu_conv2 = RepVGGBlock(
-            int(in_channels[0] * width), int(in_channels[0] * width), 3, 2, deploy=deploy
+        self.bu_conv2 = CSPLayer(
+            int(in_channels[0] * width), int(in_channels[0] * width), 3, 2
         )
         self.C3_n3 = CSPLayer(
             int(2 * in_channels[0] * width),
@@ -671,8 +671,8 @@ class YOLOPAFPN_Rep(YOLOPAFPN):
         )
 
         # bottom-up conv
-        self.bu_conv1 = RepVGGBlock(
-            int(in_channels[1] * width), int(in_channels[1] * width), 3, 2, deploy=deploy
+        self.bu_conv1 = CSPLayer(
+            int(in_channels[1] * width), int(in_channels[1] * width), 3, 2
         )
         self.C3_n4 = CSPLayer(
             int(2 * in_channels[1] * width),

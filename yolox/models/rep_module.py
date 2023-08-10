9668aee665411f393e2d399a602a41f1108173db
naihai78 <757877430@qq.com>
2023-08-10 10:08:35 +0800

    "常规训练"

--- yolox/models/rep_module.py
+++ yolox/models/rep_module.py
@@ -573,7 +573,7 @@ class DiverseBranchBlock(nn.Module):
         #     self.nonlinear = nn.Identity()
         # else:
         #     self.nonlinear = nonlinear
-        self.nonlinear = nn.SiLU(inplace=True)
+        self.nonlinear = nn.SiLU()
 
         self.kernel_size = kernel_size
         self.out_channels = out_channels

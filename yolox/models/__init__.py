aead463e15c9a083006a4b9126e872178a0e5421
naihai78 <757877430@qq.com>
2023-11-09 16:47:19 +0800

    "yolox-tiny coco格式训练主动毫米波图像数据集"

--- yolox/models/__init__.py
+++ yolox/models/__init__.py
@@ -7,5 +7,5 @@ from .darknet import CSPDarknet, Darknet
 from .losses import IOUloss
 from .yolo_fpn import YOLOFPN
 from .yolo_head import YOLOXHead
-from .yolo_pafpn import YOLOPAFPN, YOLOPAFPN_Rep
+from .yolo_pafpn import YOLOPAFPN
 from .yolox import YOLOX

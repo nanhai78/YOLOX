dcae6d13a7eef8a362bbc0671eefddcf05cf7f75
naihai78 <757877430@qq.com>
2023-11-09 21:55:14 +0800

    "yolox-tiny coco格式训练主动毫米波图像数据集"

--- tools/train.py
+++ tools/train.py
@@ -14,8 +14,8 @@ from yolox.core import launch
 from yolox.exp import Exp, check_exp_value, get_exp
 from yolox.utils import configure_module, configure_nccl, configure_omp, get_num_devices
 
-# import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
 def make_parser():

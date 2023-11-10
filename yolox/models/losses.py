3fec33a30e9487e475ba8ca97487d2dd709ae026
naihai78 <757877430@qq.com>
2023-11-10 14:38:10 +0800

    "yolox-tiny coco格式训练主动毫米波图像数据集"

--- yolox/models/losses.py
+++ yolox/models/losses.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 
 class IOUloss(nn.Module):
-    def __init__(self, reduction="none", loss_type="iou"):
+    def __init__(self, reduction="none", loss_type="ciou"):
         super(IOUloss, self).__init__()
         self.reduction = reduction
         self.loss_type = loss_type
@@ -44,6 +44,39 @@ class IOUloss(nn.Module):
             area_c = torch.prod(c_br - c_tl, 1)
             giou = iou - (area_c - area_u) / area_c.clamp(1e-16)
             loss = 1 - giou.clamp(min=-1.0, max=1.0)
+        elif self.loss_type == 'ciou':
+            c_tl = torch.min(
+                (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
+            )
+            c_br = torch.max(
+                (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
+            )
+
+            # 最大外界矩形对角线长度c^2
+            w_c = (c_br - c_tl)[:, 0]
+            h_c = (c_br - c_tl)[:, 1]
+            c = w_c ** 2 + h_c ** 2
+            # 中心点距离平方d^2
+            w_d = (pred[:, :2] - target[:, :2])[:, 0]
+            h_d = (pred[:, :2] - target[:, :2])[:, 1]
+            d = w_d ** 2 + h_d ** 2
+            # 求diou
+
+            diou = iou - d / c
+
+            w_gt = target[:, 2]
+            h_gt = target[:, 3]
+            w = pred[:, 2]
+            h = pred[:, 3]
+
+            with torch.no_grad():
+                arctan = torch.atan(w_gt / h_gt) - torch.atan(w / h)
+                v = (4 / (math.pi ** 2)) * torch.pow(arctan, 2)
+                s = 1 - iou
+                alpha = v / (s + v)
+
+            ciou = diou - alpha * v
+            loss = 1 - ciou.clamp(min=-1.0, max=1.0)
 
         if self.reduction == "mean":
             loss = loss.mean()
